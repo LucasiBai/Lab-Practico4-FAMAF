e0c5141f3d2a01c1de91a930f790dff3b4f48671
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-05-29 17:56:13 -0300

    creacion de funciones de las props

--- practico4.hs
+++ practico4.hs
@@ -39,3 +39,49 @@ triangulo (f, c, t) = f == Triangulo
 
 tam :: Figura -> Int
 tam (f, c, t) = t
+
+-- Props
+
+propA :: [Figura] -> Bool
+propA [] = True
+propA (x:xs) = rojo x && propA xs
+
+propB :: [Figura] -> Bool
+propB [] = True
+propB (x:xs) = tam x < 5 && propB xs
+
+propC :: [Figura] -> Bool
+propC [] = True
+propC (x:xs) = (not (triangulo x) || rojo x) && propC xs
+
+propD :: [Figura] -> Bool
+propD [] = False
+propD (x:xs) = cuadrado x && verde x || propD xs
+
+propE :: [Figura] -> Bool
+propE [] = True
+propE (x:xs) = (not (circulo x) || (azul x && tam x < 10)) && propE xs
+
+propF :: [Figura] -> Bool
+propF [] = True
+propF (x:xs) = (not (triangulo x) || not (azul x)) && propF xs
+
+propG :: [Figura] -> Bool
+propG [] = True
+propG (x:xs) =((not (circulo x) || (not (amarillo x) && not (verde x))) && propG xs)
+
+
+propH :: [Figura] -> Bool
+propH [] = False
+propH (x:xs) = (cuadrado x && tam x < 5) || propH xs
+
+hayCirRojos :: [Figura] -> Bool
+hayCirRojos [] = False
+hayCirRojos (x:xs) = (circulo x && rojo x) || hayCirRojos xs
+
+hayCuaRojos :: [Figura] -> Bool
+hayCuaRojos [] = False
+hayCuaRojos (x:xs) = (cuadrado x && rojo x) || hayCuaRojos xs
+
+propI :: [Figura] -> Bool
+propI xs = not (hayCirRojos xs) || hayCuaRojos xs
